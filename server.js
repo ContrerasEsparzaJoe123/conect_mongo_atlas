4db9e3a42a5107ba99c4842f0dc7765776515668
ContrerasEsparzaJoe123 <47192243+ContrerasEsparzaJoe123@users.noreply.github.com>
2021-03-11 19:44:11 -0600

    port again

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const connectDB = require('./DB/Conncection');
+const express = require("express");
+const connectDB = require("./DB/Conncection");
 const app = express();
 
 connectDB();
 app.use(express.json({ extended: false }));
-app.use('/api/userModel', require('./Api/User'));
-const Port = process.env.Port ;
+app.use("/api/userModel", require("./Api/User"));
+const Port = process.env.Port;
 
-app.listen(Port, () => console.log(`Server started at port ${Port}`))
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started at port ${Port}`);
+});
