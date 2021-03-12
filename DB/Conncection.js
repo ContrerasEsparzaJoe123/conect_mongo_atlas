cd76b782f21831bfef20f44bef1348b6e92900fe
ContrerasEsparzaJoe123 <47192243+ContrerasEsparzaJoe123@users.noreply.github.com>
2021-03-11 19:32:00 -0600

    uri

--- DB/Conncection.js
+++ DB/Conncection.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI =
-  "mongodb+srv://joec:that1guy2@cluster0.tlgse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URI ="mongodb+srv://joec:that1guy2@cluster0.tlgse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   await mongoose.connect(URI, {
